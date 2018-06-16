f5e089474a4ab2a54d9d568fb71cb50d791eab56
alexstouffer <bizkite@hotmail.com>
2018-06-16 13:44:33 -0700

    added search functions, currently prints to console.log

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+
 import BusinessList from './components/BusinessList/BusinessList';
 import SearchBar from './components/SearchBar/SearchBar';
 
@@ -25,12 +25,15 @@ const businesses = [
   business
 ]
 
-class App extends Component {
+class App extends React.Component {
+  searchYelp(term, location, sortBy) {
+    console.log(`Searching Yelp with ${term}, ${location}, ${sortBy}`);
+  }
   render() {
     return (
       <div className="App">
         <h1>ravenous</h1>
-        <SearchBar />
+        <SearchBar searchYelp={this.searchYelp} />
         <BusinessList businesses={businesses} /> 
       </div>
     );
--- src/components/BusinessList/BusinessList.js
+++ src/components/BusinessList/BusinessList.js
@@ -2,12 +2,16 @@ import React from 'react';
 import './BusinessList.css';
 import Business from '../Business/Business';
 
+//businessKey is used to avoid a console error: each child should have its own unique key.
+let businessKey = 0;
+
 class BusinessList extends React.Component{
     render(){
         return <div className="BusinessList">
             {
                 this.props.businesses.map(business => {
-                    return <Business business={business} />
+                    businessKey += 1;
+                    return <Business business={business} key={businessKey}/>
                 })
             }
         </div>
